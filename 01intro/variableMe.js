2523b49a01a052e7ff7c25fa45347b67f3f5df67
jaykumar2004 <jay859119@gmail.com>
2025-09-02 12:49:54 +0530

    TS

---
+++ 01intro/variableMe.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//string
+var name = "jay";
+console.log(name);
+//number
+var num = 356485.45;
+//or we can write
+var num2;
+num2 = 4545665;
+//this is not the good practice
+//boolean
+var value = false;
+value.valueOf;
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,17 @@
+//string
+let name: string = "jay";
+console.log(name);
+export {}; //it basically removes the error
+
+//number
+let num: number = 356485.45;
+
+
+//or we can write
+let num2:number
+num2 = 4545665;
+//this is not the good practice
+
+//boolean
+let value: boolean = false;
+value.valueOf
